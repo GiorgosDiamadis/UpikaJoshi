a99e797ec757d8061e122dd2f5c9849f5585f1c4
GiorgosDiamadis <diamantisgiorgos2@gmail.com>
2022-06-01 07:05:20 +0300

    commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Node.Js script for package update
+
+
+Given a list of Github repositories and the package you want to update,
+this scripts checks if the specified version is greater than or equal to the version on each repository.
+
+For example given this data:
+
+| Repo                    | Repository |
+|:------------------------|:----------:|
+| dyte-js-sample-app      |   https://github.com/dyte-in/javascript-sample-app    |
+| dyte-sample-app-backend |    https://github.com/dyte-in/backend-sample-app    |
+| dyte-react-sample-app          |   https://github.com/dyte-in/react-sample-app/    |
+
+and the command:  node .\commands.js -update -i input.csv web-vitals@3.9.0 it gives the following result
+
+| Repo                          |                   Repository                     | version | version_satisfied |                      update_pr                      |
+|:------------------------------|:------------------------------------------------:|:-------:|:-----------------:|:---------------------------------------------------:|
+| dyte-js-sample-app            | https://github.com/dyte-in/javascript-sample-app |   N/A   |      false        |                                                     |
+| dyte-sample-app-backend       |  https://github.com/dyte-in/backend-sample-app   |   N/A   |       false       |                                                     |
+| dyte-react-sample-app         |   https://github.com/dyte-in/react-sample-app/   |  1.0.1  |       false       | https://github.com/dyte-in/react-sample-app/pull/30 |
+
+Specify the option -update if you want to create a pull request.
+The csv file is given after the parameter -i and must be in the same directory.
--- commands.js
+++ commands.js
@@ -1,122 +1,142 @@
 var program = require('commander');
 var fs = require('fs');
-var crypto = require('crypto')
+var crypto = require('crypto');
 
 var Table = require('cli-table');
-var axios = require('axios')
+var axios = require('axios');
 const {Octokit} = require("@octokit/core");
 const {createPullRequest} = require("octokit-plugin-create-pull-request");
 const MyOctokit = Octokit.plugin(createPullRequest);
 
-const TOKEN = "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"; // create token at https://github.com/settings/tokens/new?scopes=repo
+const TOKEN = "YOUR API TOKEN";
 const octokit = new MyOctokit({
     auth: TOKEN,
 });
 
 program.version('1.0').description('Version Checker')
+
 var table = new Table({
     head: ['name', 'repo', 'version', 'version_satisfied', 'update_pr']
 });
-const parseCSVData = (csv) => {
-    var data = csv.split("\r\n")
-    var csvParsed = [];
-
-
-    for (let i = 1; i < data.length; i++) {
-        csvParsed[i - 1] = data[i].split(",");
-        csvParsed[i - 1].length = data[i].length + 2;
-    }
-
-    return data;
-}
 
 program
     .argument('<csvFile>', 'The csv file you want to check the versions. Must be in the same directory.')
     .argument('<pkg>', 'The package that you want to check, must be in this format <name>@<x.y.z>.')
     .option('-update', 'Create a pull request in order to update the package on the repos that it is needed.')
-    .option('-i', 'Followed by the name of the input file, must be csv and in the same directory.')
+    .option('-i', 'Follow this with the name of the input file, must be csv and in the same directory.')
     .action(async (csvFile, pkg, options) => {
+        console.log(options)
+        if (!options.i){
+
+        }
         var data = fs.readFileSync(csvFile, "utf-8");
-        data = parseCSVData(data)
         data = data.split("\r\n")
 
-        for (let i = 1; i < data.length; i++) {
-            data[i] = data[i].split(",");
-            data[i].length = data[i].length + 2;
-
-            const repoTmp = data[i][1].split("/");
-            const repo = repoTmp[repoTmp.length - 1]
-            const user = repoTmp[repoTmp.length - 2]
-
-            const pkgGit = await axios.get(`https://api.github.com/repos/${user}/${repo}/contents/package.json`, {
-                headers: {"Authorization": ` Token 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`}
-            });
-            const buff = Buffer.from(pkgGit.data.content, 'base64');
-
-            const str = buff.toString('utf-8');
-            var packageJSON = JSON.parse(str);
-            var desired = pkg.split('@');
-
-            data[i][2] = packageJSON.dependencies[desired[0]] ? packageJSON.dependencies[desired[0]].replaceAll('^', '').replaceAll('^', '') : "N/A";
-            if (data[i][2] === 'N/A') {
-                data[i][3] = 'false';
-                table.push(data[i]);
-                continue;
-            }
-            var k = parseInt(desired[1].replaceAll('.', ''));
-            var l = parseInt(packageJSON.dependencies[desired[0]].replaceAll('^', '').replaceAll('.', ''));
-            if (k <= l) {
-
-                data[i][3] = "true";
-            } else {
-
-                if (options.Update) {
-
-                    // try {
-                    //     var shasum = crypto.createHash('sha1')
-                    //     shasum.update(`update-${desired[0]}`);
-                    //
-                    //     await axios.post(`https://api.github.com/repos/${user}/${repo}/git/refs`, {
-                    //         "ref": `refs/heads/update-${desired[0]}`, "sha": shasum.digest('hex'), "force": true
-                    //     }, {
-                    //         headers: {"Authorization": ` Token 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`}
-                    //     });
-                    //
-                    // } catch (e) {
-                    //
-                    // }
-
-                    const commitText = `Update package ${desired[0]} from version ${desired[1]} to ${packageJSON.dependencies[desired[0]]}`;
-                    packageJSON.dependencies[desired[0]] = desired[1];
-                    // try {
-                    //     const pr = await octokit
-                    //         .createPullRequest({
-                    //             owner: user,
-                    //             repo: repo,
-                    //             title: "pull request title2",
-                    //             body: "pull request description",
-                    //             head: `update-${desired[0]}`,
-                    //             changes: [
-                    //                 {
-                    //                     files: {
-                    //                         "package.json": JSON.stringify(packageJSON)
-                    //                     },
-                    //                     commit:
-                    //                        commitText,
-                    //                 },
-                    //             ],
-                    //         })
-                    //     data[i].length = 5;
-                    //     data[i][4] = `${data[i][1]}/pull/${pr.data.number}`
-                    // } catch (e) {
-                    // }
-
-
-                }
-                data[i][3] = "false";
-            }
-            table.push(data[i]);
-        }
+        // for (let i = 1; i < data.length; i++) {
+        //     data[i] = data[i].split(",");
+        //     data[i].length = data[i].length + 2;
+        //
+        //     const repoTmp = data[i][1].split("/");
+        //     const repo = repoTmp[repoTmp.length - 1]
+        //     const user = repoTmp[repoTmp.length - 2]
+        //
+        //     /**
+        //      * Get package.json from the repository. The package should in the root directory of the repo.
+        //      */
+        //     const pkgGit = await axios.get(`https://api.github.com/repos/${user}/${repo}/contents/package.json`, {
+        //         headers: {"Authorization": ` Token ${TOKEN}`}
+        //     });
+        //
+        //
+        //     /**
+        //      * Decode package.json and parse it to a JavaScript object
+        //      * @type {Buffer}
+        //      */
+        //     const buff = Buffer.from(pkgGit.data.content, 'base64');
+        //     const str = buff.toString('utf-8');
+        //     var packageJSON = JSON.parse(str);
+        //
+        //     /**
+        //      * The package to check. desired[0] -> package name, desired[1] -> package version
+        //      */
+        //     var desired = pkg.split('@');
+        //
+        //
+        //     /**
+        //      * Check if it exists,if not print N/A and go to next csv entry
+        //      * @type {string|string}
+        //      */
+        //     data[i][2] = packageJSON.dependencies[desired[0]] ? packageJSON.dependencies[desired[0]].replaceAll('^', '').replaceAll('^', '') : "N/A";
+        //     if (data[i][2] === 'N/A') {
+        //         data[i][3] = 'false';
+        //         table.push(data[i]);
+        //         continue;
+        //     }
+        //
+        //     /**
+        //      * Check version compatibility. Remove all . and ^ and concatenate numbers to form an integer which will be used for comparison
+        //      * This is why we need this format for the package <name>@<x.y.z>
+        //      * @type {number}
+        //      */
+        //     var k = parseInt(desired[1].replaceAll('.', ''));
+        //     var l = parseInt(packageJSON.dependencies[desired[0]].replaceAll('^', '').replaceAll('.', ''));
+        //     if (k <= l) {
+        //
+        //         data[i][3] = "true";
+        //     } else {
+        //
+        //         if (options.Update) {
+        //             try {
+        //
+        //                 /**
+        //                  * Create a reference in order to create a pull request.
+        //                  * @type {Hash}
+        //                  */
+        //                 var shasum = crypto.createHash('sha1')
+        //                 shasum.update(`update-${desired[0]}`);
+        //                 await axios.post(`https://api.github.com/repos/${user}/${repo}/git/refs`, {
+        //                     "ref": `refs/heads/update-${desired[0]}`, "sha": shasum.digest('hex'), "force": true
+        //                 }, {
+        //                     headers: {"Authorization": ` Token ${TOKEN}`}
+        //                 });
+        //
+        //             } catch (e) {
+        //
+        //             }
+        //
+        //             const commitText = `Update package ${desired[0]} from version ${packageJSON.dependencies[desired[0]]} to ${desired[1]}`;
+        //
+        //             /**
+        //              * Update package.json and create a pull request.
+        //              */
+        //             packageJSON.dependencies[desired[0]] = desired[1];
+        //             try {
+        //                 const pr = await octokit
+        //                     .createPullRequest({
+        //                         owner: user,
+        //                         repo: repo,
+        //                         title: `Update ${desired[0]}`,
+        //                         body: `${desired[0]} needs to be updated`,
+        //                         head: `update-${desired[0]}`,
+        //                         changes: [
+        //                             {
+        //                                 files: {
+        //                                     "package.json": JSON.stringify(packageJSON)
+        //                                 },
+        //                                 commit:
+        //                                 commitText,
+        //                             },
+        //                         ],
+        //                     })
+        //                 data[i].length = 5;
+        //                 data[i][4] = `${data[i][1]}/pull/${pr.data.number}`
+        //             } catch (e) {
+        //             }
+        //         }
+        //         data[i][3] = "false";
+        //     }
+        //     table.push(data[i]);
+        // }
 
         console.log(table.toString())
     });
