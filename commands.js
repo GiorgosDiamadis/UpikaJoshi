2ce6b7b6cf85098d4384f8ed8acf0529bc4ef697
GiorgosDiamadis <diamantisgiorgos2@gmail.com>
2022-06-01 07:05:52 +0300

    commit

--- commands.js
+++ commands.js
@@ -32,111 +32,111 @@ program
         var data = fs.readFileSync(csvFile, "utf-8");
         data = data.split("\r\n")
 
-        // for (let i = 1; i < data.length; i++) {
-        //     data[i] = data[i].split(",");
-        //     data[i].length = data[i].length + 2;
-        //
-        //     const repoTmp = data[i][1].split("/");
-        //     const repo = repoTmp[repoTmp.length - 1]
-        //     const user = repoTmp[repoTmp.length - 2]
-        //
-        //     /**
-        //      * Get package.json from the repository. The package should in the root directory of the repo.
-        //      */
-        //     const pkgGit = await axios.get(`https://api.github.com/repos/${user}/${repo}/contents/package.json`, {
-        //         headers: {"Authorization": ` Token ${TOKEN}`}
-        //     });
-        //
-        //
-        //     /**
-        //      * Decode package.json and parse it to a JavaScript object
-        //      * @type {Buffer}
-        //      */
-        //     const buff = Buffer.from(pkgGit.data.content, 'base64');
-        //     const str = buff.toString('utf-8');
-        //     var packageJSON = JSON.parse(str);
-        //
-        //     /**
-        //      * The package to check. desired[0] -> package name, desired[1] -> package version
-        //      */
-        //     var desired = pkg.split('@');
-        //
-        //
-        //     /**
-        //      * Check if it exists,if not print N/A and go to next csv entry
-        //      * @type {string|string}
-        //      */
-        //     data[i][2] = packageJSON.dependencies[desired[0]] ? packageJSON.dependencies[desired[0]].replaceAll('^', '').replaceAll('^', '') : "N/A";
-        //     if (data[i][2] === 'N/A') {
-        //         data[i][3] = 'false';
-        //         table.push(data[i]);
-        //         continue;
-        //     }
-        //
-        //     /**
-        //      * Check version compatibility. Remove all . and ^ and concatenate numbers to form an integer which will be used for comparison
-        //      * This is why we need this format for the package <name>@<x.y.z>
-        //      * @type {number}
-        //      */
-        //     var k = parseInt(desired[1].replaceAll('.', ''));
-        //     var l = parseInt(packageJSON.dependencies[desired[0]].replaceAll('^', '').replaceAll('.', ''));
-        //     if (k <= l) {
-        //
-        //         data[i][3] = "true";
-        //     } else {
-        //
-        //         if (options.Update) {
-        //             try {
-        //
-        //                 /**
-        //                  * Create a reference in order to create a pull request.
-        //                  * @type {Hash}
-        //                  */
-        //                 var shasum = crypto.createHash('sha1')
-        //                 shasum.update(`update-${desired[0]}`);
-        //                 await axios.post(`https://api.github.com/repos/${user}/${repo}/git/refs`, {
-        //                     "ref": `refs/heads/update-${desired[0]}`, "sha": shasum.digest('hex'), "force": true
-        //                 }, {
-        //                     headers: {"Authorization": ` Token ${TOKEN}`}
-        //                 });
-        //
-        //             } catch (e) {
-        //
-        //             }
-        //
-        //             const commitText = `Update package ${desired[0]} from version ${packageJSON.dependencies[desired[0]]} to ${desired[1]}`;
-        //
-        //             /**
-        //              * Update package.json and create a pull request.
-        //              */
-        //             packageJSON.dependencies[desired[0]] = desired[1];
-        //             try {
-        //                 const pr = await octokit
-        //                     .createPullRequest({
-        //                         owner: user,
-        //                         repo: repo,
-        //                         title: `Update ${desired[0]}`,
-        //                         body: `${desired[0]} needs to be updated`,
-        //                         head: `update-${desired[0]}`,
-        //                         changes: [
-        //                             {
-        //                                 files: {
-        //                                     "package.json": JSON.stringify(packageJSON)
-        //                                 },
-        //                                 commit:
-        //                                 commitText,
-        //                             },
-        //                         ],
-        //                     })
-        //                 data[i].length = 5;
-        //                 data[i][4] = `${data[i][1]}/pull/${pr.data.number}`
-        //             } catch (e) {
-        //             }
-        //         }
-        //         data[i][3] = "false";
-        //     }
-        //     table.push(data[i]);
-        // }
+        for (let i = 1; i < data.length; i++) {
+            data[i] = data[i].split(",");
+            data[i].length = data[i].length + 2;
+
+            const repoTmp = data[i][1].split("/");
+            const repo = repoTmp[repoTmp.length - 1]
+            const user = repoTmp[repoTmp.length - 2]
+
+            /**
+             * Get package.json from the repository. The package should in the root directory of the repo.
+             */
+            const pkgGit = await axios.get(`https://api.github.com/repos/${user}/${repo}/contents/package.json`, {
+                headers: {"Authorization": ` Token ${TOKEN}`}
+            });
+
+
+            /**
+             * Decode package.json and parse it to a JavaScript object
+             * @type {Buffer}
+             */
+            const buff = Buffer.from(pkgGit.data.content, 'base64');
+            const str = buff.toString('utf-8');
+            var packageJSON = JSON.parse(str);
+
+            /**
+             * The package to check. desired[0] -> package name, desired[1] -> package version
+             */
+            var desired = pkg.split('@');
+
+
+            /**
+             * Check if it exists,if not print N/A and go to next csv entry
+             * @type {string|string}
+             */
+            data[i][2] = packageJSON.dependencies[desired[0]] ? packageJSON.dependencies[desired[0]].replaceAll('^', '').replaceAll('^', '') : "N/A";
+            if (data[i][2] === 'N/A') {
+                data[i][3] = 'false';
+                table.push(data[i]);
+                continue;
+            }
+
+            /**
+             * Check version compatibility. Remove all . and ^ and concatenate numbers to form an integer which will be used for comparison
+             * This is why we need this format for the package <name>@<x.y.z>
+             * @type {number}
+             */
+            var k = parseInt(desired[1].replaceAll('.', ''));
+            var l = parseInt(packageJSON.dependencies[desired[0]].replaceAll('^', '').replaceAll('.', ''));
+            if (k <= l) {
+
+                data[i][3] = "true";
+            } else {
+
+                if (options.Update) {
+                    try {
+
+                        /**
+                         * Create a reference in order to create a pull request.
+                         * @type {Hash}
+                         */
+                        var shasum = crypto.createHash('sha1')
+                        shasum.update(`update-${desired[0]}`);
+                        await axios.post(`https://api.github.com/repos/${user}/${repo}/git/refs`, {
+                            "ref": `refs/heads/update-${desired[0]}`, "sha": shasum.digest('hex'), "force": true
+                        }, {
+                            headers: {"Authorization": ` Token ${TOKEN}`}
+                        });
+
+                    } catch (e) {
+
+                    }
+
+                    const commitText = `Update package ${desired[0]} from version ${packageJSON.dependencies[desired[0]]} to ${desired[1]}`;
+
+                    /**
+                     * Update package.json and create a pull request.
+                     */
+                    packageJSON.dependencies[desired[0]] = desired[1];
+                    try {
+                        const pr = await octokit
+                            .createPullRequest({
+                                owner: user,
+                                repo: repo,
+                                title: `Update ${desired[0]}`,
+                                body: `${desired[0]} needs to be updated`,
+                                head: `update-${desired[0]}`,
+                                changes: [
+                                    {
+                                        files: {
+                                            "package.json": JSON.stringify(packageJSON)
+                                        },
+                                        commit:
+                                        commitText,
+                                    },
+                                ],
+                            })
+                        data[i].length = 5;
+                        data[i][4] = `${data[i][1]}/pull/${pr.data.number}`
+                    } catch (e) {
+                    }
+                }
+                data[i][3] = "false";
+            }
+            table.push(data[i]);
+        }
 
         console.log(table.toString())
     });
